5e2d6ca9d39f9eff00ff6b6d162f9490bdcd46f4
jufrin01 <jufrinaha111@gmail.com>
2024-09-02 11:00:29 +0700

    lean css html

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,29 @@
+let BtnSubmit = document.getElementById('btn_submit');
 
+let charts = [];
+
+function subminHandler() {
+
+    let id;
+    if(charts.length === 0 ){
+        id = 1;
+    }else {
+        id = charts[charts.length - 1].id + 1;
+    }
+
+    let barangValue = document.getElementById('barang').value;
+    // let namaValue = document.getElementById('nama').value;
+    let hargaValue = document.getElementById('harga').value;
+
+    let tempObj = {
+        id ,
+        nama: barangValue,
+        harga: +hargaValue
+    }
+    charts.push(tempObj);
+    console.log(charts);
+}
+
+BtnSubmit.addEventListener('click', subminHandler);
+
+console.log(BtnSubmit);
